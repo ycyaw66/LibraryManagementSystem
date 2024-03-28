537ebd0939d17f609a8ccc28778429a0d3f43926
ycyaw66 <497410282@qq.com>
2024-03-28 11:11:16 +0800

    implement frontend

--- librarymanagementsystem-frontend/src/components/Book.vue
+++ librarymanagementsystem-frontend/src/components/Book.vue
@@ -3,7 +3,7 @@
     <el-scrollbar height="100%" style="width: 100%; height: 100%; ">
         <div style="margin-top: 20px; margin-left: 40px; font-size: 2em; font-weight: bold; ">图书管理</div>    
         <p style="margin-left: 40px; margin-top: 50px;">这里是图书管理页面，请尝试实现。</p>
-
+        
     </el-scrollbar>
 </template>
 
